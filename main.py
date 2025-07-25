effa86a11ab5eb70ff0fd6f306501579cdc72ef9
depood <depood.dev@tuta.com>
2025-07-25 20:35:54 +0200

    add circleshape base class

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -17,6 +19,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
